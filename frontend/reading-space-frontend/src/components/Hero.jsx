bf1452e4b2d8432a21bb427d62947061f798092e
Princy-039 <princyvarghese22@gmail.com>
2025-01-08 23:09:33 +0530

    Image Changed

--- frontend/reading-space-frontend/src/components/Hero.jsx
+++ frontend/reading-space-frontend/src/components/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
     <section id="home" className="bg-light py-5 mt-5">
       <div className="container py-5">
         <div className="row align-items-center">
-          <div className="col-lg-6">
-            <h1 className="display-4 fw-bold">Welcome to ReadingSpace</h1>
+          <div className="col-lg-5">
+            <h1 className="display-5 fw-bold">Welcome to ReadingSpace</h1>
             <p className="lead">Your perfect reading environment awaits</p>
             
             {/* Link to /login page */}
@@ -17,7 +17,7 @@ const Hero = () => {
           </div>
           <div className="col-lg-6">
             <img
-              src="https://img.freepik.com/free-photo/friends-library-table_23-2147678958.jpg"
+              src="https://images.pexels.com/photos/9489906/pexels-photo-9489906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Women Reading a Book"
               className="img-fluid rounded shadow"
             />
