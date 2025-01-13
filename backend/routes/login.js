9e5da7885af654767104ec3964cac428ff872204
Nikhil Sam Mani <nikhilsm1996@gmail.com>
2025-01-13 19:38:32 +0530

    returns seatAssigned in login response

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -32,7 +32,16 @@ router.post('/', async (req, res) => {
         const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
 
         // Send the token to the client
-        res.status(200).json({ message: 'Login successful',user, token });
+        res.status(200).json({
+            message: 'Login successful',
+            user: {
+                email: user.email,
+                name: user.name,
+                role: user.role,
+                seatAssigned: user.seatAssigned, // Include the seatAssigned field
+            },
+            token,
+        });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'An error occurred during login.' });
